b94e9669f8dd3ce48e7ff116c29f98fc662da542
Sakesh120 <sakeshchopkar927@gmail.com>
2025-12-29 21:25:00 +0530

    complete

--- Count_Down_Timer/app.js
+++ Count_Down_Timer/app.js
@@ -1,7 +1,18 @@
-// const result = document.getElementById("result");
+const result = document.getElementById("result");
 
-const current = Date.now();
+setInterval(() => {
+  const current = Date.now();
 
-const olympicTime = new Date(2028, 6, 14).getTime(); //// months start from 0 , therefore for july use 6
+  const olympicTime = new Date(2026, 0, 1).getTime(); //// months start from 0 , therefore for january use 6
+  let timer = olympicTime - current;
+  const day = Math.floor(timer / (1000 * 60 * 60 * 24));
+  timer %= 1000 * 60 * 60 * 24;
+  const hours = Math.floor(timer / (1000 * 60 * 60));
+  timer %= 1000 * 60 * 60;
+  const minutes = Math.floor(timer / (1000 * 60));
+  timer %= 1000 * 60;
+  const seconds = Math.floor(timer / 1000);
+  timer %= 1000;
 
-console.log(olympicTime);
+  result.textContent = `${day}:Days   ${hours}:Hour  ${minutes}:Minutes  ${seconds}:Seconds`;
+}, 1000);
